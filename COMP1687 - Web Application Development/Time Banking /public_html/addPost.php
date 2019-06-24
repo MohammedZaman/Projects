27562988b6fe1363e67c63004f81c1ca217696d4
MohammedZaman <46172370+MohammedZaman@users.noreply.github.com>
2019-06-24 21:28:08 +0100

    Delete addPost.php

--- COMP1687 - Web Application Development/Time Banking /public_html/addPost.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'core/init.php';
-include 'includes/head.php';
-include 'includes/Navigations/AddPostNavigation.php';
-include 'content/addPost.php';
-include 'includes/footer.php';
-?>
