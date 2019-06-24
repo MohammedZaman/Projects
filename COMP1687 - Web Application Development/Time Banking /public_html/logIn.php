6f1313903be2387b38df73acfd244a9669db7997
MohammedZaman <46172370+MohammedZaman@users.noreply.github.com>
2019-06-24 21:28:35 +0100

    Delete logIn.php

--- COMP1687 - Web Application Development/Time Banking /public_html/logIn.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'core/init.php';
-include 'includes/head.php';
-include 'includes/Navigations/LogInNavigation.php';
-include 'content/logIn.php';
-include 'includes/footer.php';
-
-?>
