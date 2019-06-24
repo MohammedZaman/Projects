0d25c5368a458897ee70706147c324408783c46e
MohammedZaman <46172370+MohammedZaman@users.noreply.github.com>
2019-06-24 21:28:01 +0100

    Delete ViewPost.php

--- COMP1687 - Web Application Development/Time Banking /public_html/ViewPost.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'core/init.php';
-include 'includes/head.php';
-include 'includes/Navigations/ViewPostNavigation.php';
-include 'content/ViewPost.php';
-include 'includes/footer.php';
-
-?>
