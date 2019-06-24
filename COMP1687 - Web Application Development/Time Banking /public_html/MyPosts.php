a92c15b42d607c75edae6c44b3f911f29d721d71
MohammedZaman <46172370+MohammedZaman@users.noreply.github.com>
2019-06-24 21:27:39 +0100

    Delete MyPosts.php

--- COMP1687 - Web Application Development/Time Banking /public_html/MyPosts.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'core/init.php';
-include 'includes/head.php';
-include 'includes/Navigations/MyPostNavigation.php';
-include 'content/MyPosts.php';
-include 'includes/footer.php';
-
-?>
